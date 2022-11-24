e164795901b6d43ab2b3c86a5407a90a69b7b2e0
smartragul94 <ragul.raj94@gmail.com>
2022-11-24 21:02:55 +0530

    Task one

---
+++ src/org/samplegit/Samplegit.java
@@ -0,0 +1,14 @@
+package org.samplegit;
+
+public class Samplegit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Samplegit g1 = new Samplegit();
+		 
+		System.out.println("Master code open");
+		
+			
+	}
+}
